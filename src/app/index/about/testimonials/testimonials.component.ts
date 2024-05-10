53a3b35cf51e3a875d36bfb1542c077e49ec3018
yasirsaleemvws <147795322+yasirsaleemvws@users.noreply.github.com>
2024-05-10 10:57:50 +0500

    fixes

--- src/app/index/about/testimonials/testimonials.component.ts
+++ src/app/index/about/testimonials/testimonials.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./testimonials.component.scss']
 })
 export class TestimonialsComponent implements OnInit {
-  count = [1, 2, 3, 4]
+  count = [
+    {
+      name: 'Jay Sanders',
+      sr_no: 'MC # 104970',
+      description: "Place my trucking business in Hawkeye Freights & Dispatch's hands and haven't needed another dispatcher since. Outstanding Communication, very knowledgeable of the business, and they don't break the bank on percentage. Worth way more than other companies charging 10+ percent. Thank you, guys. "
+    },
+    {
+      name: 'Sam Jose',
+      sr_no: 'MC# 717422',
+      description: "Working with Hawkeye Freights & Dispatch has been a game-changer for our trucking business. Their personalized approach, attention to detail, and commitment to excellence have truly set them apart in the industry. The team at Hawkeye goes above and beyond to ensure our trucks are matched with the right loads, keeping our operations running smoothly and efficiently."
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
