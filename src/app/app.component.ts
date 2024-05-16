a5e9a85fac7a3890ba09c2d23caccf31139af92d
yasirsaleemvws <126661150+Yasir365@users.noreply.github.com>
2024-05-16 21:26:26 +0500

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,30 @@ import * as AOS from 'aos';
 
 @Component({
   selector: 'app-root',
-  template: `<router-outlet></router-outlet> `,
+  template: `
+  <div class="watermark">
+  <p>Under Development</p>
+  </div>
+  <router-outlet></router-outlet> 
+  `,
+  styles: [
+    `
+    .watermark {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    opacity: 0.5; /* Adjust opacity as needed */
+    z-index: 1000; /* Ensure it appears above other content */
+  }
+
+  .watermark p {
+    font-size: 5rem; /* Adjust font size as needed */
+    font-weight: 900;
+    color: blue; /* Adjust color as needed */
+  }
+    `
+  ]
 })
 export class AppComponent implements OnInit {
   title = 'HawkEye';
